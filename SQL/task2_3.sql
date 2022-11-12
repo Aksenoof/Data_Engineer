a03fd27a8b90ee3b14afffee2f1ee1d52d828583
Aksenoof <aksenoof@gmail.com>
2022-11-12 18:28:59 +0500

    добавил task2_3, task2_4

---
+++ SQL/task2_3.sql
@@ -0,0 +1,177 @@
+CREATE TYPE level_type AS ENUM ('jun', 'middle', 'senior', 'lead');
+CREATE TYPE grade_type AS ENUM ('A', 'B', 'C', 'D', 'E');
+
+CREATE TABLE IF NOT EXISTS employees(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	fullname VARCHAR(50) NOT NULL,
+	dob DATE NOT NULL,
+	start_date DATE NOT NULL,
+	post VARCHAR(50) NOT NULL,
+	level level_type NOT NULL,
+	salary INT,
+	department_id INT,
+	driver_license BOOLEAN,
+    CONSTRAINT department_fk
+        FOREIGN KEY (department_id)
+        REFERENCES departments(id)
+        ON DELETE CASCADE)
+
+CREATE TABLE IF NOT EXISTS departments(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	manager VARCHAR(50),
+	number_employees SMALLINT)
+
+CREATE TABLE IF NOT EXISTS scores(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	employee_id INT,
+	q1 grade_type,
+	q2 grade_type,
+	q3 grade_type,
+	q4 grade_type,
+	CONSTRAINT employee_fk
+        FOREIGN KEY (employee_id)
+        REFERENCES employees(id)
+        ON DELETE CASCADE)
+
+INSERT INTO departments(
+	name,
+	manager,
+	number_employees
+)
+VALUES
+	('developments', 'Иванов Н.П.', 6),
+	('marketing', 'Фролова Ю.Н.', 2),
+	('интеллектуальный анализ данных', 'Смирнов А.П.', 3);
+
+
+INSERT INTO employees(
+	fullname,
+	dob,
+	start_date,
+	post,
+	level,
+	salary,
+	department_id,
+	driver_license
+)
+VALUES
+	('Иванов Н.П.', '1980/05/12', '2010/02/13', 'руководитель отдела', 'lead', 385000, 1, true),
+	('Петро Н.Ю.', '1985/04/22', '2012/07/23', 'программист python', 'senior', 310000, 1, true),
+	('Сидоров С.С.', '1988/07/10', '2015/08/17', 'программист python', 'middle', 255000, 1, true),
+	('Степанов И.В.', '1990/10/02', '2020/01/13', 'программист python', 'jun', 85000, 1, false),
+	('Николаев В.В.', '1978/01/25', '2014/03/03', 'back-end разработчик', 'senior', 285000, 1, true),
+	('Цой В.Р.', '1980/03/04', '2015/11/11', 'back-end разработчик', 'middle', 215000, 1, false),
+	('Пушкин С.А.', '1991/05/12', '2021/12/09', 'back-end разработчик', 'jun', 115000, 1, false),
+	('Фролова Ю.Н.', '1987/04/12', '2017/03/23', 'руководитель отдела', 'lead', 358000, 2, true),
+	('Дмитреев К.Ю.', '1981/06/16', '2015/06/13', 'web-программист', 'senior', 210000, 2, true),
+	('Злобин В.Ю.', '1988/07/27', '2019/04/14', 'web-программист', 'middle', 198000, 2, true),
+	('Смирнов А.П.', '1983/03/28', '2021/01/11', 'руководитель отдела', 'lead', 347000, 3, true),
+	('Зайцев И.Н.', '1977/05/25', '2021/02/28', 'data scientist', 'senior', 276000, 3, true),
+	('Волков Н.П.', '1980/01/23', '2021/02/28', 'data scientist', 'middle', 203000, 3, false),
+	('Сорока М.Ю.', '1990/09/03', '2021/04/03', 'data scientist', 'jun', 98000, 3, true);
+
+
+INSERT INTO scores(
+	employee_id,
+	q1,
+	q2,
+	q3,
+	q4
+)
+VALUES
+	(1, 'C', 'B', 'C', 'B'),
+	(2, 'C', 'C', 'C', 'B'),
+	(3, 'B', 'B', 'D', 'B'),
+	(4, 'B', 'A', 'C', 'E'),
+	(5, 'C', 'C', 'C', 'B'),
+	(6, 'C', 'C', 'C', 'C'),
+	(7, 'B', 'B', 'C', 'B'),
+	(8, 'C', 'B', 'D', 'B'),
+	(9, 'C', 'D', 'C', 'B'),
+	(10, 'C', 'B', 'C', 'B'),
+	(11, 'D', 'B', 'B', 'E'),
+	(12, 'D', 'B', 'C', 'B'),
+	(13, 'C', 'C', 'C', 'B'),
+	(14, 'C', 'B', 'B', 'B');
+
+
+-- вывод id, фамилию и стаж сотрудника всего списка
+SELECT id, fullname, AGE(CURRENT_DATE, start_date) AS  experience
+FROM employees e;
+
+-- вывод id, фамилию и стаж сотрудника первых трех сотрудников
+SELECT id, fullname, AGE(CURRENT_DATE, start_date) AS  experience
+FROM employees e ORDER BY id LIMIT 3;
+
+-- список сотрудников водителей
+SELECT id 
+FROM employees e 
+WHERE driver_license=true;
+
+-- id сотрудников, которые хотя бы за 1 квартал получили оценку D или E
+SELECT employee_id 
+FROM scores s 
+WHERE (grade='D' OR grade='E');
+
+--масксимальная зарплата в компании
+SELECT MAX(salary) 
+FROM employees e;
+
+-- название самого крупного отдела
+SELECT name 
+FROM departments d 
+ORDER BY name LIMIT 1;
+
+SELECT name 
+FROM departments d 
+WHERE number_employees=(SELECT MAX(number_employees) 
+						FROM departments d);
+
+-- номера сотрудников от самых опытных до вновь прибывших
+SELECT id, level 
+FROM employees e
+ORDER BY CASE WHEN level = 'lead' THEN 1
+			WHEN level = 'senior' THEN 2
+			WHEN level = 'middle' THEN 3
+			WHEN level = 'jun' THEN 4
+			END;
+
+-- средняя зарплата для каждого уровня сотрудников
+SELECT level, ROUND(AVG(salary),2) AS mean 
+FROM employees e 
+GROUP BY level 
+ORDER BY mean DESC;
+
+-- коэффициент годовой премии
+ALTER TABLE employees ADD COLUMN bonus NUMERIC
+
+UPDATE employees e
+SET bonus = (SELECT 1 + quarter1 + quarter2 + quarter3 + quarter4 AS bonus_year 
+			FROM (SELECT s.*, CASE WHEN q1 = 'A' THEN 0.2
+								WHEN q1 = 'B' THEN 0.1
+								WHEN q1 = 'C' THEN 0
+								WHEN q1 = 'D' THEN -0.1
+								WHEN q1 = 'E' THEN -0.2
+								else 1 END AS quarter1,
+							  CASE WHEN q2 = 'A' THEN 0.2
+								WHEN q2 = 'B' THEN 0.1
+								WHEN q2 = 'C' THEN 0
+								WHEN q2 = 'D' THEN -0.1
+								WHEN q2 = 'E' THEN -0.2
+								else 1 END AS quarter2,
+							  CASE WHEN q3 = 'A' THEN 0.2
+								WHEN q3 = 'B' THEN 0.1
+								WHEN q3 = 'C' THEN 0
+								WHEN q3 = 'D' THEN -0.1
+								WHEN q3 = 'E' THEN -0.2
+								else 1 END AS quarter3,
+							   CASE WHEN q4 = 'A' THEN 0.2
+								WHEN q4 = 'B' THEN 0.1
+								WHEN q4 = 'C' THEN 0
+							    WHEN q4 = 'D' THEN -0.1
+								WHEN q4 = 'E' THEN -0.2
+								else 1 END AS quarter4 
+				FROM scores s) AS b_q
+			WHERE e.id = b_q.id								
+			);
---
+++ SQL/task2_4.sql
@@ -0,0 +1,176 @@
+-- фамилия сотрудника с самой высокой зарплатой
+SELECT fullname
+FROM employees e
+WHERE salary = (SELECT MAX(salary) 
+				FROM employees e
+                );
+
+-- фамилии сотрудников в алфавитном порядке
+SELECT fullname 
+FROM employees e 
+ORDER BY fullname;
+
+-- средний стаж для каждого уровня сотрудников
+SELECT level, AVG(AGE(CURRENT_DATE, start_date)) AS experience_mean
+FROM employees e
+GROUP BY level
+ORDER BY experience_mean;
+
+-- фамилия сотрудника и название отдела, в котором он работает
+SELECT fullname, name AS department 
+FROM employees e
+LEFT JOIN departments d
+ON e.department_id = d.id;
+
+-- название отдела и фамилия сотрудника с самой высокой зарплатой в данном отделе 
+SELECT name AS department, fullname, salary
+FROM employees e
+LEFT JOIN departments d
+ON e.department_id = d.id
+WHERE salary = (SELECT MAX(salary) 
+				FROM employees e
+                );
+
+--название отдела, сотрудники которого получат наибольшую премию по итогам года
+SELECT name AS department
+FROM employees e
+LEFT JOIN departments d
+ON e.department_id = d.id
+WHERE bonus = (SELECT MAX(bonus) 
+			   FROM employees e
+               );
+
+-- индексация зарплат сотрудников с учетом коэффициента премии
+WITH 
+jun AS (SELECT d.id,COUNT(level) AS jun FROM employees e 
+JOIN departments d
+ON e.department_id = d.id
+WHERE level = 'jun'
+GROUP BY d.name, d.id),
+
+middle AS (SELECT d.id, COUNT(*) AS middle FROM employees e 
+JOIN departments d
+ON e.department_id = d.id
+WHERE level = 'middle'
+GROUP BY d.name, d.id),
+
+senior AS (SELECT d.id, COUNT(*) AS senior FROM employees e 
+JOIN departments d
+ON e.department_id = d.id
+WHERE level = 'senior'
+GROUP BY d.name, d.id),
+
+lead AS (SELECT d.id, COUNT(*) AS lead FROM employees e 
+JOIN departments d
+ON e.department_id = d.id
+WHERE level = 'lead'
+GROUP BY d.name, d.id),
+
+sum_index_salary  AS (SELECT department_id AS id, SUM(((salary * b) + salary)) AS sum_index_salary
+FROM (SELECT *, CASE WHEN bonus >= 1.2 THEN 0.2
+					 WHEN (1 < bonus AND bonus < 1.2)  THEN 0.1
+			    else 0 END AS b
+      FROM employees e
+      ) AS b_y 
+GROUP BY department_id),
+
+grade_A AS (SELECT department_id, SUM(quarter1) + SUM(quarter2) + SUM(quarter3) + SUM(quarter4) AS grade_A
+FROM(SELECT *, CASE WHEN q1 = 'A' THEN 1
+			   ELSE 0 END quarter1,
+	 		   CASE WHEN q2 = 'A' THEN 1
+			   ELSE 0 END quarter2,
+	 	 	   CASE WHEN q3 = 'A' THEN 1
+			   ELSE 0 END quarter3,
+	 	 	   CASE WHEN q4 = 'A' THEN 1
+			   ELSE 0 END quarter4
+	FROM scores s) AS b_q 
+RIGHT JOIN employees e 
+ON b_q.employee_id = e.id
+GROUP BY department_id),
+
+grade_B AS (SELECT department_id, SUM(quarter1) + SUM(quarter2) + SUM(quarter3) + SUM(quarter4) AS grade_B
+FROM(SELECT *, CASE WHEN q1 = 'B' THEN 1
+			   ELSE 0 END quarter1,
+	 		   CASE WHEN q2 = 'B' THEN 1
+			   ELSE 0 END quarter2,
+	 	 	   CASE WHEN q3 = 'B' THEN 1
+			   ELSE 0 END quarter3,
+	 	 	   CASE WHEN q4 = 'B' THEN 1
+			   ELSE 0 END quarter4
+	FROM scores s) AS b_q 
+RIGHT JOIN employees e 
+ON b_q.employee_id = e.id
+GROUP BY department_id),
+
+grade_C AS (SELECT department_id, SUM(quarter1) + SUM(quarter2) + SUM(quarter3) + SUM(quarter4) AS grade_C
+FROM(SELECT *, CASE WHEN q1 = 'C' THEN 1
+			   ELSE 0 END quarter1,
+	 		   CASE WHEN q2 = 'C' THEN 1
+			   ELSE 0 END quarter2,
+	 	 	   CASE WHEN q3 = 'C' THEN 1
+			   ELSE 0 END quarter3,
+	 	 	   CASE WHEN q4 = 'C' THEN 1
+			   ELSE 0 END quarter4
+	FROM scores s) AS b_q 
+RIGHT JOIN employees e 
+ON b_q.employee_id = e.id
+GROUP BY department_id),
+
+grade_D AS (SELECT department_id, SUM(quarter1) + SUM(quarter2) + SUM(quarter3) + SUM(quarter4) AS grade_D
+FROM(SELECT *, CASE WHEN q1 = 'D' THEN 1
+			   ELSE 0 END quarter1,
+	 		   CASE WHEN q2 = 'D' THEN 1
+			   ELSE 0 END quarter2,
+	 	 	   CASE WHEN q3 = 'D' THEN 1
+			   ELSE 0 END quarter3,
+	 	 	   CASE WHEN q4 = 'D' THEN 1
+			   ELSE 0 END quarter4
+	FROM scores s) AS b_q 
+RIGHT JOIN employees e 
+ON b_q.employee_id = e.id
+GROUP BY department_id),
+
+grade_E AS (SELECT department_id, SUM(quarter1) + SUM(quarter2) + SUM(quarter3) + SUM(quarter4) AS grade_E
+FROM(SELECT *, CASE WHEN q1 = 'E' THEN 1
+			   ELSE 0 END quarter1,
+	 		   CASE WHEN q2 = 'E' THEN 1
+			   ELSE 0 END quarter2,
+	 	 	   CASE WHEN q3 = 'E' THEN 1
+			   ELSE 0 END quarter3,
+	 	 	   CASE WHEN q4 = 'E' THEN 1
+			   ELSE 0 END quarter4
+	FROM scores s) AS b_q 
+RIGHT JOIN employees e 
+ON b_q.employee_id = e.id
+GROUP BY department_id),
+
+sum_bonus AS (SELECT department_id, SUM(salary * bonus) AS sum_bonus 
+FROM  employees e
+GROUP BY department_id),
+
+total_salary AS (SELECT department_id, SUM(salary + (salary * bonus)) AS total_salary 
+FROM  employees e
+GROUP BY department_id)
+
+SELECT name AS department, manager, number_employees, AVG(AGE(CURRENT_DATE, start_date)) AS experience_mean,
+ROUND(AVG(salary),2) AS salary_mean, COALESCE(NULLIF(jun, null), 0) AS jun, COALESCE(NULLIF(middle, null), 0) AS middle,
+COALESCE(NULLIF(senior, null), 0) AS senior, COALESCE(NULLIF(lead, null), 0) AS  lead, SUM(salary) AS  sum_salary, 
+sum_index_salary, grade_A, grade_B, grade_C, grade_D, grade_E, ROUND(AVG(bonus),2) AS bonus_coef_mean, sum_bonus, total_salary, 
+sum_index_salary + sum_bonus AS total_salary_index
+FROM employees e 
+LEFT JOIN departments d ON e.department_id = d.id
+LEFT JOIN jun j ON e.department_id = j.id
+LEFT JOIN middle m ON e.department_id = m.id
+LEFT JOIN senior s ON e.department_id = s.id
+LEFT JOIN lead l ON e.department_id = l.id
+LEFT JOIN sum_index_salary sis ON e.department_id = sis.id
+LEFT JOIN grade_A g_A ON e.department_id = g_A.department_id
+LEFT JOIN grade_B g_B ON e.department_id = g_B.department_id
+LEFT JOIN grade_C g_C ON e.department_id = g_C.department_id
+LEFT JOIN grade_D g_D ON e.department_id = g_D.department_id
+LEFT JOIN grade_E g_E ON e.department_id = g_E.department_id
+LEFT JOIN sum_bonus s_b ON e.department_id = s_b.department_id
+LEFT JOIN total_salary t_s ON e.department_id = t_s.department_id
+GROUP BY department, manager, number_employees, jun, middle, senior, lead, sum_index_salary, grade_A, grade_B, grade_C,
+grade_D, grade_E, sum_bonus, total_salary
+ORDER BY department
